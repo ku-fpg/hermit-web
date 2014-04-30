d8009fd617b8d0493daa099835d70ad60ae301f5
Andrew Farmer <anfarmer@ku.edu>
2014-04-30 12:42:37 -0500

    Use HERMIT's driver appropriately

--- driver/Main.hs
+++ driver/Main.hs
@@ -3,22 +3,16 @@ module Main where
 import HERMIT.Driver
 
 import System.Environment
-import System.Process
-import System.Exit
+
+import Data.Version
+import Paths_hermit_web as P
+
+hermitWebVersion :: String
+hermitWebVersion = "HERMIT Web v" ++ showVersion P.version
 
 main :: IO ()
 main = do
-    (file_nm:_) <- getArgs -- TODO: real option parser
-    putStrLn $ "[starting " ++ hermit_version ++ " web service on " ++ file_nm ++ "]"
+    (file_nm:rest) <- getArgs 
+    putStrLn $ "[starting " ++ hermitWebVersion ++ " on " ++ file_nm ++ "]"
 
-    let cmds = file_nm : ghcFlags ++ plugin_cmds
-        plugin_cmds = ["-fplugin=HERMIT.Web"
-                      ,"-package ghc"]
-    putStrLn $ "% ghc " ++ unwords cmds
-    ex <- runWithArgs "ghc" cmds
-    exitWith ex
-
-runWithArgs :: String -> [String] -> IO ExitCode
-runWithArgs cmd args = do
-    (_,_,_,r) <- createProcess $ proc cmd args
-    waitForProcess r
+    hermitDriver $ file_nm : "-opt=HERMIT.Web" : rest
