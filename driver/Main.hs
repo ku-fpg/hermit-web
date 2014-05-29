35258910eee416116daa500f72a5eedb65ce7c65
RyanGlScott <ryan.gl.scott@ku.edu>
2014-05-29 16:21:14 -0500

    Print usage with no args

--- driver/Main.hs
+++ driver/Main.hs
@@ -1,18 +1,26 @@
+{-# LANGUAGE LambdaCase #-}
 module Main where
 
 import HERMIT.Driver
 
 import System.Environment
 
+import Data.List
+import Data.List.Split
 import Data.Version
 import Paths_hermit_web as P
 
 hermitWebVersion :: String
-hermitWebVersion = "HERMIT Web v" ++ showVersion P.version
+hermitWebVersion = "HERMIT-Web v" ++ showVersion P.version
+
+hermitWebUsage :: IO ()
+hermitWebUsage = mapM_ putStrLn [hermitWebVersion, "", replace "hermit" "hermit-web" usageOutput]
+    where replace :: String -> String -> String -> String
+          replace old new = intercalate new . splitOn old
 
 main :: IO ()
-main = do
-    (file_nm:rest) <- getArgs 
-    putStrLn $ "[starting " ++ hermitWebVersion ++ " on " ++ file_nm ++ "]"
-
-    hermitDriver $ file_nm : "-opt=HERMIT.Web" : rest
+main = getArgs >>= \case
+    (file_nm:rest) -> do
+        putStrLn $ "[starting " ++ hermitWebVersion ++ " on " ++ file_nm ++ "]"
+        hermitDriver $ file_nm : "-opt=HERMIT.Web" : rest
+    [] -> hermitWebUsage
